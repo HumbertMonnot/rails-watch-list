5792c9e0111200950a9643a1c08a050f43b03bde
humbertmonnot <humbert.monnot@gmail.com>
2022-05-20 11:44:29 +0200

    with root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :bookmarks, only: [:destroy]
   # delete "bookmarks/:id", to: "bookmarks#destroy", as: "delete_book"
-  resources :lists, only: [:index, :show, :new, :create] do
+  root to: "lists#index"
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
     resources :reviews, only: [:create]
   end
+  resources :bookmarks, only: [:destroy]
 end
